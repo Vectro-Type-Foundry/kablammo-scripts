2bdc34c147747a0cd66bbf0fc0a4085f64c26213
Travis Kochel <travis@scribbletone.com>
2020-09-05 15:41:07 -0700

    use margins

--- KablammoDashGrid/KablammoDashGrid.glyphsReporter/Contents/Resources/plugin.py
+++ KablammoDashGrid/KablammoDashGrid.glyphsReporter/Contents/Resources/plugin.py
@@ -25,23 +25,25 @@ class KablammoDashGrid(ReporterPlugin):
 		self.menuName = Glyphs.localize({'en': self.name})
 		self.keyboardShortcut = 'k'
 		self.keyboardShortcutModifier = NSControlKeyMask | NSCommandKeyMask | NSAlternateKeyMask
-		self.numRows = 10
+		self.numRows = 9
 		self.rowWidth = 100
+		self.marginTop = 79
+		self.marginBottom = 69
 		self.onColor = ( 0.0, 1.0, 0.0, 0.15 )
 		self.offColor = ( 1.0, 0.0, 0.0, 0.15 )
 	
 	@objc.python_method
 	def foreground(self, layer):
-		rowHeight = layer.master.capHeight / float(self.numRows)
+		rowHeight = (layer.master.capHeight - self.marginTop - self.marginBottom) / float(self.numRows)
 
 		for i in range(int(self.numRows)):
-			y = i * rowHeight
-			c1 = self.onColor
-			c2 = self.offColor
+			y = i * rowHeight + self.marginBottom
+			c1 = self.offColor
+			c2 = self.onColor			
 
 			if (i % 2 == 0):
-				c1 = self.offColor
-				c2 = self.onColor
+				c1 = self.onColor
+				c2 = self.offColor
 
 			self.drawRow(0, y, self.rowWidth, rowHeight, c1)
 			self.drawRow(layer.width - self.rowWidth, y, self.rowWidth, rowHeight, c2)
