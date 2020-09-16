680d8bb3e5b2ce62739920cc345d2d90badf4ca6
Ethan Cohen <ethan@ethancohenstudio.com>
2020-09-16 16:17:25 +0200

    Add CopySelectionToOtherMasters.py

---
+++ CopySelectionToOtherMasters.py
@@ -0,0 +1,21 @@
+#MenuTitle: Copy selection to other masters
+# -*- coding: utf-8 -*-
+__doc__="""
+Propagate selected paths to all other masters.
+"""
+
+# Ethan Cohen. Updated 9/16/2020.
+
+import GlyphsApp
+import copy
+
+f = Glyphs.font
+l = Font.selectedLayers[0]
+g = l.parent
+
+for layer in g.layers:
+	for p in l.paths:
+		if p.selected:
+			layer.paths.append(p.copy())
+
+Glyphs.redraw()
