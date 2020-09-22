cf494f0d4feb13367c64aeefacfad7e405f3a55f
Travis Kochel <travis@scribbletone.com>
2020-09-22 15:49:18 -0700

    delete selected path, and keep index with copy selection

--- CopySelectionToOtherMasters.py
+++ CopySelectionToOtherMasters.py
@@ -14,9 +14,9 @@ l = Font.selectedLayers[0]
 g = l.parent
 
 for layer in g.layers:
-	if layer != l:
-		for p in l.paths:
-			if p.selected:
-				layer.paths.append(p.copy())
+  if layer != l:
+    for i in range(len(l.paths)):
+      if l.paths[i].selected:
+        layer.paths.insert(i, l.paths[i].copy())
 
 Glyphs.redraw()
---
+++ DeleteSelectionFromOtherMasters.py
@@ -0,0 +1,34 @@
+#MenuTitle: Delete selection from other masters
+# -*- coding: utf-8 -*-
+__doc__="""
+Delete selected paths from all other masters.
+"""
+
+import GlyphsApp
+
+currentLayer = Font.selectedLayers[0]
+glyph = currentLayer.parent
+masterIds = map(lambda m: m.id, Font.masters) 
+
+def getSelectedContourIndexes():
+  indexes = []
+  for i in range(len(currentLayer.paths)):
+    if currentLayer.paths[i].selected:
+      indexes.append(i)
+
+  return sorted(indexes, reverse=True)
+
+def deleteIndexesInLayer(indexes, layer):
+  for i in indexes:
+    del(layer.paths[i])
+
+def main():
+  selectedIndexes = getSelectedContourIndexes()
+  
+  for masterId in masterIds:
+    deleteIndexesInLayer(selectedIndexes, glyph.layers[masterId])
+
+
+main()
+
+Glyphs.redraw()
