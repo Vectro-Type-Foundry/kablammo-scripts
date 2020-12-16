6f2dab36697e68fe8c1d95ac246d1c51cfc5b27c
Travis Kochel <travis@scribbletone.com>
2020-12-15 19:08:49 -0800

    flip special glyphs

--- GenerateReverseAlts.py
+++ GenerateReverseAlts.py
@@ -6,8 +6,18 @@ Rebuild reverse variable alternates from base glyphs
 from GlyphsApp import *
 from Foundation import *
 
-sourceGlyphNames = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z', 'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+sourceGlyphNames = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z', 'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'border_9']
 
+ignoreInFeatures = ['border_9']
+
+specialGlyphs = [
+  {
+    'name': 'border_9', 
+    'altName': 'border_10',
+    'unicode': 'E009',
+    'flip': True
+  }
+]
 altSuffix = '.rev'
 
 class GenerateReverseAlts(object):
@@ -33,6 +43,7 @@ class GenerateReverseAlts(object):
 
     newGlyph = sourceGlyph.copy()
     newGlyph.name = altGlyphName
+    newGlyph.unicode = ''
     Glyphs.font.glyphs.append(newGlyph)
 
   def copyGlyph(self, sourceLayerMeta, sourceLayerOutlines, targetLayer):
@@ -94,17 +105,55 @@ class GenerateReverseAlts(object):
     skipCode = " ".join(map(lambda x: "@skip", range(skip)))
     code = "lookup " + lookupName + " {\n"
     for sourceGlyphName in sourceGlyphNames:
-      code += "  sub " + sourceGlyphName + " " + skipCode + " " + sourceGlyphName + "\' by " + sourceGlyphName + altSuffix + ";\n"
+      if sourceGlyphName not in ignoreInFeatures:
+        code += "  sub " + sourceGlyphName + " " + skipCode + " " + sourceGlyphName + "\' by " + sourceGlyphName + altSuffix + ";\n"
     code += "} " + lookupName + ";\n"
     return code
 
+  def rotateLayer(self, layer, degrees=0.0, xOrigin=0.0, yOrigin=0.0):
+    layer.transform_checkForSelection_doComponents_( 
+      rotationTransform(angle=degrees, xOrigin=xOrigin, yOrigin=yOrigin),
+      False,
+      True
+    )
+  
+  def flipGlyph(self, glyph):
+    print 'flip'
+    for layer in glyph.layers:
+      x = (layer.bounds.size.width * 0.5) + layer.bounds.origin.x
+      y = (layer.bounds.size.height * 0.5) + layer.bounds.origin.y
+      self.rotateLayer(layer, 180, x, y)
+
+  def specialGlyphs(self):
+    for specialGlyph in specialGlyphs:
+      existingGlyph = Glyphs.font[specialGlyph['altName']]
+      if existingGlyph:
+        del(Glyphs.font.glyphs[existingGlyph.name])
+        
+      glyph = Glyphs.font[specialGlyph['name'] + altSuffix]
+      glyph.name = specialGlyph['altName']
+      glyph.unicode = specialGlyph['unicode']
+
+      if specialGlyph['flip']:
+        self.flipGlyph(glyph)
+
   def run(self):
     for sourceGlyphName in sourceGlyphNames:
+      print sourceGlyphName
       self.duplicatesourceGlyph(sourceGlyphName)
       self.reverseMasters(sourceGlyphName)
       self.reverseSpecialLayers(sourceGlyphName)
-
     self.updateFeatureCode()
+    self.specialGlyphs()
 
     Glyphs.redraw()
 
+
+
+def rotationTransform( angle=180.0, xOrigin=0.0, yOrigin=0.0 ):
+    """Returns a TransformStruct for rotating."""
+    RotationTransform = NSAffineTransform.transform()
+    RotationTransform.translateXBy_yBy_( xOrigin, yOrigin )
+    RotationTransform.rotateByDegrees_( angle )
+    RotationTransform.translateXBy_yBy_( -xOrigin, -yOrigin )
+    return RotationTransform
